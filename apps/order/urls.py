3f9c5863436eaac804a94c75e3185d33d9801686
backend-developer-hojiakbar <soyibnazarovhoji@gmail.com>
2024-06-19 11:59:05 +0500

    first commit

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import TestView
-# from apps.order.views import GeneratePayLinkAPIView
+from apps.order.views import successfully_payment
 
 
 urlpatterns = [
     path('payme/endpoint/', TestView.as_view(),),
     # path('pay-link/', GeneratePayLinkAPIView.as_view(), name='generate-pay-link')
+    path('payme/successfully_payment/', successfully_payment, name='succesfully-payment')
 ]
